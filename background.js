3091265ea0cb8cb21fc18f49854bfc4780996a68
wangxinwei <wangxinwei101@pingan.com.cn>
2015-10-26 18:19:15 +0800

    add iframe advertisement branch

--- background.js
+++ background.js
@@ -2,46 +2,22 @@
  * Author: wxwdesign@gmail.com
  * Source: https://github.com/wahaha2012/ads-cleaner-extension
  */
-(function () {
-    var contextMenuId,
-        cleanStartUpMenuId;
-    chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-        if(request.showContextMenu){
-            if(!contextMenuId){
-                contextMenuId = chrome.contextMenus.create({
-                    "title" : "Clean Advertisements",
-                    "documentUrlPatterns" : [
-                        "*://*.iteye.com/*",
-                        "*://*.eastmoney.com/*",
-                        "*://blog.sina.com.cn/*",
-                        "*://*.stock.hexun.com/*",
-                        "*://*.chinaz.com/*"
-                    ],
-                    "onclick" : sendCleanMsg
-                });
-            }
-            //clean startup menu
-            if(!cleanStartUpMenuId){
-                cleanStartUpMenuId = chrome.contextMenus.create({
-                    "title" : "Clean Startup Company",
-                    "documentUrlPatterns" : [
-                        "*://*.eastmoney.com/*"
-                    ],
-                    "onclick" : sendCleanStMsg
-                });
-            }
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.browserAction.getBadgeText({tabId: tab.id}, function(text){
+        var display = 'none',
+            status = 'ON';
+            
+        if(text == 'ON') {
+            status = '';
+            display = 'block';
         }
+
+        chrome.browserAction.setBadgeText({
+            text: status,
+            tabId: tab.id
+        });
+        chrome.tabs.executeScript({
+            code: 'Array.prototype.forEach.call(document.querySelectorAll("iframe"), function(item){item.style.display="'+display+'"})'
+        });
     });
-
-    function sendCleanMsg(info, tab){
-        chrome.tabs.sendMessage(tab.id, {
-            cleanAds: true
-        });
-    }
-
-    function sendCleanStMsg(info, tab){
-        chrome.tabs.sendMessage(tab.id, {
-            cleanStartup: true
-        });
-    }
-})();
+});
